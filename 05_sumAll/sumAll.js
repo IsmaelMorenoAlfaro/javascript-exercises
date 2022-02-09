3f2aa7527516b8690ad2f5716b266eb4d3cad908
Ismael Moreno Alfaro <isma.elx84@gmail.com>
2022-02-09 21:25:59 +0100

    Change sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+    const invalidNumber1 = (number1<0) || !Number.isInteger(number1);
+    const invalidNumber2 = (number2<0) || !Number.isInteger(number2);
+    let sum = 0;
 
+    if (invalidNumber1 || invalidNumber2){
+        return "ERROR";
+    }
+
+    if (number1 < number2){
+        for (let i=number1; i<=number2; i++){
+            sum += i;
+        }
+    }
+    else if (number1 > number2){
+        for (let i=number1; i>=number2; i--){
+            sum += i;
+        }
+    }
+    return sum; 
 };
 
 // Do not edit below this line
