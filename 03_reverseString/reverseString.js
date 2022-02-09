0e5ebfeb9b6a4540778efd499ece954ccfbd56fa
Ismael Moreno Alfaro <isma.elx84@gmail.com>
2022-02-09 14:44:00 +0100

    Change reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let newString = "";
+    const words = string.split("");
+    for (let i=words.length-1; i>=0; i--){
+        newString+= words[i];
+    }
+    return newString;
 };
 
 // Do not edit below this line
