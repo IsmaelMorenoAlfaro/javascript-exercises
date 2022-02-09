316f3472d9b2c69599a12feada3cfd2a9be799d9
Ismael Moreno Alfaro <isma.elx84@gmail.com>
2022-02-09 20:18:49 +0100

    Change RemoveFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elements) {
+    
+    for (let i=0; i<array.length; i++){
+        for (let j=0; j<elements.length; j++){
+            if (array[i] === elements[j]){
+                array.splice(i,1);
+                i = 0;
+            }
+        }
+    }
+    
+    return array;
 };
 
 // Do not edit below this line
