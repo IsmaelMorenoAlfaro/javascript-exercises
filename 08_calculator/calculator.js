60fc79babb8f8f657ce79ddc5eff70bc4907a656
Ismael Moreno Alfaro <isma.elx84@gmail.com>
2022-02-23 21:36:13 +0100

    add javascript calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+return numbers.reduce((total,num) => total += num , 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.length 
+    ? numbers.reduce((total, number) => total * number)
+    : 0;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let factorial = 1;
+  if (number !== 0) {
+    for (let i=1; i<=number; i++){
+      factorial *= i;
+    }
+  }
+	return factorial;
 };
 
 // Do not edit below this line
