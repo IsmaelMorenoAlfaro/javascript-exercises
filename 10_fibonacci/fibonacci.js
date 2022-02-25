911a9583e8d9a202ee1606badc2707169e23eb2e
Ismael Moreno Alfaro <isma.elx84@gmail.com>
2022-02-25 12:23:18 +0100

    Add javascript function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let a = 0,
+        b = 1;
+    if (number < 0) return "OOPS";
+    if (number === 0) return a;
+    for (let i=1; i<number; i++){
+        let aux = b;
+        b = a + b;
+        a = aux;
+    }
+    return b;
 };
 
 // Do not edit below this line
