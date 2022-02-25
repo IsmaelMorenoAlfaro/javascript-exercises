64caffff9e86f8f0e0ca5949f04f4be8b9cacd48
Ismael Moreno Alfaro <isma.elx84@gmail.com>
2022-02-25 12:25:36 +0100

    add javascript functions

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((older, person) => {
+      const olderAge = getAge(older.yearOfBirth, older.yearOfDeath);  
+      const currentAge = getAge(person.yearOfBirth, person.yearOfDeath);
+      return olderAge > currentAge ? older : person;
+    });
 };
+
+const getAge = ((birthYear, deathYear) =>{
+  if (!deathYear) deathYear = new Date().getFullYear();
+  return deathYear - birthYear;
+});
+  
 
 // Do not edit below this line
 module.exports = findTheOldest;
